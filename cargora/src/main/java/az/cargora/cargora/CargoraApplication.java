3f54d4f4ec9aa99c6c08dceb7a919524518fc3c1
Sevinc <sevincquliyeva000@gmail.com>
2026-04-08 14:55:51 +0400

    project created

---
+++ cargora/src/main/java/az/cargora/cargora/CargoraApplication.java
@@ -0,0 +1,13 @@
+package az.cargora.cargora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoraApplication.class, args);
+	}
+
+}
---
+++ cargora/src/test/java/az/cargora/cargora/CargoraApplicationTests.java
@@ -0,0 +1,13 @@
+package az.cargora.cargora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
