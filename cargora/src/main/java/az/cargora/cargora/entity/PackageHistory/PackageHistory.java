283462af4fbbd4f80618b20d0f64c90c66bf577b
Sevinc <sevincquliyeva000@gmail.com>
2026-04-09 18:40:08 +0400

    deleted getter/setter

--- cargora/src/main/java/az/cargora/cargora/entity/PackageHistory/PackageHistory.java
+++ cargora/src/main/java/az/cargora/cargora/entity/PackageHistory/PackageHistory.java
@@ -1,10 +1,15 @@
 package az.cargora.cargora.entity.PackageHistory;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "packageHistory")
+@Getter
+@Setter
 public class PackageHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,30 +30,5 @@ public class PackageHistory {
         this.timestamp = timestamp;
 
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public PackageStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(PackageStatus status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
 
 }
