cb206fde7b3d7178b1de0a100ed4a9e11d40c95a
Sevinc <sevincquliyeva000@gmail.com>
2026-04-09 17:51:39 +0400

    userEntity created

---
+++ cargora/src/main/java/az/cargora/cargora/entity/Fullname.java
@@ -0,0 +1,11 @@
+package az.cargora.cargora.entity;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Fullname {
+
+    private String name;
+    private String surname;
+
+}
--- cargora/src/main/java/az/cargora/cargora/entity/User.java
+++ cargora/src/main/java/az/cargora/cargora/entity/User.java
@@ -1,6 +1,41 @@
 package az.cargora.cargora.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
 public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String PIN;
+
+    @Embedded
+    private Fullname fullname;
+
+    @ManyToOne()
+    @JoinColumn(name = "pickup_point_id")
+    private PickUpPoint pickUpAdress;
+
+    private String homeAddress;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Package> packages;
+
 
 
 }
