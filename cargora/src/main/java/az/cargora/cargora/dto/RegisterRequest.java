817c73c78e60ebd443cc6ae8bf9c518474f4cd5e
Sevinc <sevincquliyeva000@gmail.com>
2026-04-18 23:46:46 +0400

    security(without JWT)

---
+++ cargora/src/main/java/az/cargora/cargora/dto/RegisterRequest.java
@@ -0,0 +1,13 @@
+package az.cargora.cargora.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+    private String email;
+    private String password;
+
+    private String PIN;
+}
