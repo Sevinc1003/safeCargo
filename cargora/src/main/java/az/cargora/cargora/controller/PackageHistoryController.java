e3fb7cc1fa22c93dc5826da07e499e930e892033
Sevinc <sevincquliyeva000@gmail.com>
2026-05-02 10:58:57 +0400

    correcting some endpoints

--- cargora/src/main/java/az/cargora/cargora/controller/PackageHistoryController.java
+++ cargora/src/main/java/az/cargora/cargora/controller/PackageHistoryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
 
 @RestController
-@RequestMapping("/api/package-history")
+@RequestMapping("package-history")
 @RequiredArgsConstructor
 public class PackageHistoryController {
 
