2c135effbc86733db02c99b98351f4d59689bb18
Sevinc <sevincquliyeva000@gmail.com>
2026-05-03 15:29:33 +0400

    cleaning unused imports

--- cargora/src/test/java/az/cargora/cargora/controller/BalanceControllerTest.java
+++ cargora/src/test/java/az/cargora/cargora/controller/BalanceControllerTest.java
@@ -19,7 +19,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BalanceController.class)
